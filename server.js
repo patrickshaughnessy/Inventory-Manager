3fde5d1fe6c908c17790c797176767024b82db82
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-01-12 11:51:42 -0800

    remove comments

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
-// mongoose.connect('mongodb://localhost/inventorymanager');
-// mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@ds053764.mongolab.com:53764/inventorymanager' || 'mongodb://localhost/inventorymanager');
 var mongoURL = process.env.MONGOLAB_URI || 'mongodb://localhost/inventorymanager';
 var mongoose = require('mongoose');
 mongoose.connect(mongoURL, function(err){
